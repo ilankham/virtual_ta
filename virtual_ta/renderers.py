b6249d5a199322dc5a7889aac68063de122e1832
ilankham <isaiah.lankham@gmail.com>
2017-11-18 18:32:52 -0800

    Document package contents

--- virtual_ta/renderers.py
+++ virtual_ta/renderers.py
@@ -1,3 +1,5 @@
+"""Creates functions for rendering templates against supplied parameters"""
+
 from csv import DictReader
 import typing
 
@@ -10,6 +12,30 @@ def render_template_from_csv_file(
     *,
     key: str = None
 ) -> dict:
+    """Renders a Jinja2 template against a CSV file
+
+    This function inputs a Jinja2 template file and a CSV file whose column
+    headers correspond to the variables in the template fle (and, optionally, a
+    string specifying a column header from the CSV file; otherwise, the first
+    column in the CSV file is used) and outputs a dictionary keyed by the
+    specified column and having as values the results of rendering the template
+    against the row of the CSV file corresponding to the key value
+
+    Args:
+        template_fp: pointer to file or file-like object that is ready to read
+            from and contains a Jinja2 template
+        gradebook_fp: pointer to file or file-like object that is ready to read
+            from and contains a CSV file with columns headers in its first row
+        key: a column header from gradebook_fp, whose values should be used as
+            key values in the dictionary generated
+
+    Returns:
+        A dictionary whose keys come from the column specified by the argument
+        key (or from the first column in the CSV file, if no value is
+        specified) and whose values are the template file rendered against the
+        row in the CSV file corresponding to the key value
+
+    """
     template_text = Template(template_fp.read())
 
     gradebook_file_reader = DictReader(gradebook_fp)
--- virtual_ta/slack_bot.py
+++ virtual_ta/slack_bot.py
@@ -1,16 +1,57 @@
+"""Creates a class for interfacing with Slack Web API
+
+This module creates a class for encapsulating a Slack Account based upon API
+token, including a method for reading an API token from a file and for making
+Slack Web API calls to send messages to users in the corresponding Slack
+Workspace by username.
+
+See https://api.slack.com/web for more information about the Slack Web API.
+
+"""
+
 import requests
 import typing
 
 
 class SlackBot(object):
+    """Class for interfacing with Slack Web API"""
     def __init__(self, api_token: str = None) -> None:
+        """Initializes SlackBot object using API Token
+
+        Args:
+            api_token: a Slack API Token generated using either
+                (1) https://api.slack.com/custom-integrations/legacy-tokens to
+                    create a legacy token with full permissions scopes or
+                (2) https://api.slack.com/apps to create an internal
+                    integration app having at least the permission scopes of
+                    chat:write:user, im:read, and users:read
+
+        """
         self.api_token = api_token
 
     def set_api_token_from_file(self, fp: typing.IO) -> None:
+        """Loads Slack API Token from file
+
+        Args:
+            fp: pointer to file or file-like object that is ready to read from
+                and contains a Slack API Token generated using either
+                (1) https://api.slack.com/custom-integrations/legacy-tokens to
+                    create a legacy token with full permissions scopes or
+                (2) https://api.slack.com/apps to create an internal
+                    integration app having at least the permission scopes of
+                    chat:write:user, im:read, and users:read
+
+        """
         self.api_token = fp.readline()
 
     @property
     def user_ids(self) -> dict:
+        """Returns dictionary with username -> user id
+
+        Uses a Slack Web API call https://api.slack.com/methods/users.list
+        with no caching
+
+        """
         users_list_response = requests.post(
             url="https://slack.com/api/users.list",
             headers={
@@ -26,6 +67,12 @@ class SlackBot(object):
 
     @property
     def user_dm_channels(self) -> dict:
+        """Returns dictionary with username -> user direct message channel id
+
+        Uses a Slack Web API call https://api.slack.com/methods/im.list
+        with no caching
+
+        """
         im_list_response = requests.post(
             url="https://slack.com/api/im.list",
             headers={
@@ -45,6 +92,19 @@ class SlackBot(object):
         return return_value
 
     def direct_message_by_username(self, messages_by_username: dict) -> dict:
+        """Sends direct messages using Slack Web API calls.
+
+        Uses Slack Web API call https://api.slack.com/methods/chat.postMessage
+
+        Args:
+            messages_by_username: dictionary keyed by username with values the
+                messages to send to each user
+
+        Returns:
+            dictionary keyed by direct message channel id with values the
+                messages sent to the corresponding users
+
+        """
         user_dm_channels = self.user_dm_channels
 
         return_value = {}
